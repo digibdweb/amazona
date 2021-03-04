ebd61d16b51713aa621597a0304b4b3f23965937
Tanvir Ahmed <digibdweb@gmail.com>
2021-03-04 18:13:32 +0600

    redux added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import data from '../data'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux";
 import Rating from '../components/Rating';
 import { Link } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { detailsProduct } from '../actions/productActions';
+
 export default function ProductScreen(props) {
-    const product = data.products.find( (x) => x._id === props.match.params.id);
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector((state) => state.productDetails);
+    const { loading, error, product } = productDetails;
+    useEffect( () =>{
+        dispatch( detailsProduct(productId));
+    },[dispatch, productId] );
+
     return (
         <div>
+        { loading ? 
+        (<LoadingBox></LoadingBox>)
+        :error? (<MessageBox variant="danger" >{error}</MessageBox>)
+        :(
+            <div>
             <Link to ="/">Back to Result</Link>
         <div className="row top">
             <div className="col-2">
@@ -54,5 +70,8 @@ export default function ProductScreen(props) {
 
         </div>
         </div>
-    )
+        )
+        }
+        </div>        
+    );
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
-import data from './data';
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 const initialState = {};
-const reducer = (state,action) => {
-    return {products: data.products};
-}
+const reducer = combineReducers({
+    productList : productListReducer,
+    productDetails : productDetailsReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
